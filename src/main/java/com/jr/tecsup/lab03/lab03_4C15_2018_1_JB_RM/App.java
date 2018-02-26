ecc0a9d12add1a013ec191e7490a829518e63ba7
Alumno <Alumno@LAB701-19>
2018-02-26 09:36:20 -0500

    First Commit

---
+++ src/main/java/com/jr/tecsup/lab03/lab03_4C15_2018_1_JB_RM/App.java
@@ -0,0 +1,13 @@
+package com.jr.tecsup.lab03.lab03_4C15_2018_1_JB_RM;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
